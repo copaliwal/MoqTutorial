39ea0b873bab7cb62f908a843cabcaa188927e4c
Chandrashekhar Paliwal <copaliwal@gmail.com>
2020-05-17 22:29:46 +0530

    005 Set method return value using Mock

--- CreditCardApplications.Test/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Test/CreditCardApplicationEvaluatorShould.cs
@@ -32,5 +32,27 @@ namespace CreditCardApplications.Test
 
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
         }
+
+        [Fact]
+        public void DeclineLowIncomeApplications()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator =
+                new Mock<IFrequentFlyerNumberValidator>();
+
+            mockValidator.Setup(x => x.IsValid("xyz")).Returns(true);
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 19_999,
+                Age = 42,
+                FrequentFlyerNumber = "xyz"
+            };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+        }
     }
 }
